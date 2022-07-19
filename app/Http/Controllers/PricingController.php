3791a9cf80b45376d9d9be7ce08b1c8f84541a0d
karim-atef5397 <106876595+karim-atef5397@users.noreply.github.com>
2022-07-19 17:53:58 +0200

    update

---
+++ app/Http/Controllers/PricingController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pricing;
+use App\Http\Requests\StorePricingRequest;
+use App\Http\Requests\UpdatePricingRequest;
+
+class PricingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePricingRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePricingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pricing  $pricing
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pricing $pricing)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pricing  $pricing
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pricing $pricing)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePricingRequest  $request
+     * @param  \App\Models\Pricing  $pricing
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePricingRequest $request, Pricing $pricing)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pricing  $pricing
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pricing $pricing)
+    {
+        //
+    }
+}
